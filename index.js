a76053d926352ef9312bbbc731caa9e3be5fbc64
PirminP <pirmin.pernsteiner@gmail.com>
2022-09-07 22:24:11 -0300

    fix:verifying environment variables

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' || false;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
